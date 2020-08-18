2b3bcca07ec07d1fd2ca7b9e77b76084f2b947f6
Gerome Matilla <gerome.matilla07@gmail.com>
2020-08-18 10:48:17 +0800

    functional theme switcher

--- js/theme-switcher.js
+++ js/theme-switcher.js
@@ -26,6 +26,32 @@ class ThemeSwitcher {
 		this._themeModesArrIndex = (this._themeModesArrIndex + 1) % this._themeModesArr.length;
 	}
 
+	_applyThemeColors(themeLightHour, themeLightDark) {
+		// Apply CSS Colors based on the active theme
+		if (this._activeThemeMode == 'dark') {
+
+			// Dark mode
+			document.documentElement.setAttribute('data-theme', 'dark');
+
+		} else if (this._activeThemeMode == 'light') {
+
+			// Light mode
+			document.documentElement.setAttribute('data-theme', 'light');
+
+		} else {
+
+			// Auto mode (based on time)
+			const date = new Date();
+			const hour = date.getHours();
+
+			if (hour >= themeLightHour && hour < themeLightDark) {
+				document.documentElement.setAttribute('data-theme', 'light');
+			} else {
+				document.documentElement.setAttribute('data-theme', 'dark');
+			}
+		}
+	}
+
 	_updateThemeMode() {
 		this._activeThemeMode = this._themeModesArr[this._themeModesArrIndex];
 		let themeObject = this._themeModes[String(this._activeThemeMode)];
@@ -36,12 +62,16 @@ class ThemeSwitcher {
 		let themeLightDark = 0;
 
 		if (this._activeThemeMode === 'auto') {
-			let themeLightHour = themeObject.lightHour;
-			let themeLightDark = themeObject.darkHour;
+			themeLightHour = themeObject.lightHour;
+			themeLightDark = themeObject.darkHour;
 		}
 
+		// Update theme mode icon
 		this._buttonImageThemeSwitch.style.backgroundImage = `url('assets/theme-buttons/${themeIcon}.svg')`;
 		this._buttonImageThemeSwitch.style.backgroundSize = 'cover';
+
+		// Apply CSS Colors based on the active theme
+		this._applyThemeColors(themeLightHour, themeLightDark);
 	}
 
 	_buttonThemeSwitchClickEvent() {
