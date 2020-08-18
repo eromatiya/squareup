9dad80051b010d9339ad5ead19504de3a8c06916
Gerome Matilla <gerome.matilla07@gmail.com>
2020-08-18 16:46:23 +0800

    more cleanup

--- js/config.js
+++ js/config.js
@@ -1,6 +1,27 @@
 class Config {
 	constructor() {}
 
+	getThemeMode() {
+		const themeModes = {
+			'light': {
+				name: 'Light',
+				icon: 'light-mode'
+			},
+			'dark': {
+				name: 'Dark',
+				icon: 'dark-mode'
+			},
+			'auto': {
+				name: 'Auto',
+				icon: 'auto-mode',
+				lightHour: '7',
+				darkHour: '18'
+			}
+		}
+
+		return themeModes;
+	}
+
 	getQuickSearchData() {
 		const quickSearchData = {
 			'r/': {
@@ -27,27 +48,6 @@ class Config {
 		};
 
 		return quickSearchData;
-	}
-
-	getThemeMode() {
-		const themeModes = {
-			'light': {
-				name: 'Light',
-				icon: 'light-mode'
-			},
-			'dark': {
-				name: 'Dark',
-				icon: 'dark-mode'
-			},
-			'auto': {
-				name: 'Auto',
-				icon: 'auto-mode',
-				lightHour: '7',
-				darkHour: '18'
-			}
-		}
-
-		return themeModes;
 	}
 
 	getSearchEngines() {
